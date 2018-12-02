d7d1eed8f3eb3ff3470d63423ace95b1b7e2b71d
Prakarsh <ppandey@g.hmc.edu>
2018-12-01 21:09:24 -0800

    revise code, add OneThing Files

--- sendSteps.py
+++ sendSteps.py
@@ -1,40 +1,32 @@
 from  gpiozero import LED, LEDBoard
-from time import sleep
+from time import *
+from omxplayer.player import OMXPlayer
+from pathlib import Path
+from OneThing import *
 
 STEP3 = 19
 STEP2 = 13
 STEP1 = 6
 STEP0 = 5
 BPM_CLK = 26
-BPM = 128
 RESET_PIN = 17
 
 stepLeds = LEDBoard(STEP3, STEP2, STEP1, STEP0)
 bpmClockLed = LED(BPM_CLK)
-steps = [
-	[0, 1, 1, 0],
-	[1, 1, 1, 1],
-	[0, 0, 0, 1],
-	[1, 0, 1, 0],
-	[0, 1, 1, 0]
-]
-testStep = [
-[1,0,0,0],
-[0,0,0,0],
-[0,0,0,0],
-[0,0,0,0],
-[0,0,0,0]]
 resetLed = LED(RESET_PIN)
 
 def sendSteps():
 	# pulse reset signal before sending over steps
 	resetLed.on()
 	resetLed.off()
+	sleepAmount = 60/OneThingBPM
+	player = OMXPlayer(Path(OneThingMP3Path))
 	# send over steps
-	for step in steps:
+	for step in OneThingSteps:
+		start = time()
 		stepLeds.value = tuple(step)
 		bpmClockLed.on()
-		sleep(60/128)
 		bpmClockLed.off()
-		sleep(60/128)
+		end = time()
+		sleep(sleepAmount - (end - start))
 	stepLeds.value = tuple([0]*4)
