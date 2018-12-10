09f5cb86b757a12e58c266c5f268b1d073e7dfa2
Prakarsh <ppandey@g.hmc.edu>
2018-12-09 18:50:28 -0800

    it works

--- CapTouch.py
+++ CapTouch.py
@@ -19,15 +19,15 @@ i2c = busio.I2C(board.SCL, board.SDA)
 mpr121 = adafruit_mpr121.MPR121(i2c)
 
 # we will output touches on GPIO Pins of the Pi
-TOUCH3 = 21
-TOUCH2 = 20
-TOUCH1 = 16
-TOUCH0 = 12
+TOUCH_LEFT = 12
+TOUCH_DOWN = 16
+TOUCH_UP = 20
+TOUCH_RIGHT = 21
 
-touchOut = LEDBoard(TOUCH3, TOUCH2, TOUCH1, TOUCH0)
+touchOut = LEDBoard(TOUCH_LEFT, TOUCH_DOWN, TOUCH_UP, TOUCH_RIGHT)
 
 # Loop forever testing each input and printing when they're touched.
 while True:
-	print(mpr121[6].value, mpr121[4].value, mpr121[2].value, mpr121[0].value)
-	touchOut.value = (mpr121[6].value, mpr121[4].value, mpr121[2].value, mpr121[0].value)
+	print(mpr121[0].value, mpr121[2].value, mpr121[4].value, mpr121[6].value)
+	touchOut.value = (mpr121[0].value, mpr121[2].value, mpr121[4].value, mpr121[6].value)
 
